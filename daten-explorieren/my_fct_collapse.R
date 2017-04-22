c719030723b5d7f00dec87f88ad8a25e47e8c652
petzi53 <peter.baumgartner@donau-uni.ac.at>
2017-04-22 08:18:43 +0200

    Fehlerkorrektur in Datensatz und Recodierung

---
+++ daten-explorieren/my_fct_collapse.R
@@ -0,0 +1,23 @@
+### collapse levels of item battery
+### This function could be mor general if I would add additional parameters in the call
+### Each of this parameter is a list of two string vectors
+### the first one is the new (summarised) level,
+### the other one a string vector of levels to collapse
+my_fct_collapse <- function(df)
+        # df = data frame of factors
+        # that has to match exactly the levels of function fct_collapse
+        {
+        j = ncol(df)
+        for (i in 1:j) {
+                df[[i]] <- fct_collapse(df[[i]],
+                                        "trifft zu" = c("trifft voll zu",
+                                                        "trifft weitgehend zu",
+                                                        "trifft überwiegend zu"),
+                                        "trifft nicht zu" = c("trifft eher nicht zu",
+                                                              "trifft gar nicht zu"))
+        }
+        df
+}
+
+# example for call
+# wunsch.rec0 <- my_fct_collapse(wunsch)
